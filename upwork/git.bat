beb674588cfd46c1f6d2e9606347cd2ddcd107e2
mwadjabu <mwadjabuntacyizankimana@gmail.com>
2024-09-17 12:12:13 +0200

    Add files via upload

---
+++ upwork/git.bat
@@ -0,0 +1,38 @@
+PS C:\Users\Samlite\Desktop\upwork> git add .
+PS C:\Users\Samlite\Desktop\upwork> git status
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   h.html
+        new file:   index.html
+        new file:   script.js
+        new file:   style.css
+
+PS C:\Users\Samlite\Desktop\upwork> git commit -m "pushint to git"
+Author identity unknown
+
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+to set your account's default identity.
+Omit --global to set the identity only in this repository.
+
+fatal: unable to auto-detect email address (got 'Samlite@DESKTOP-BGNN9O9.(none)')
+PS C:\Users\Samlite\Desktop\upwork> git config --global user.email "mwadjabuntacyizankimana@gmail.com"
+PS C:\Users\Samlite\Desktop\upwork> git status
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   h.html
+        new file:   index.html
+        new file:   script.js
+        new file:   style.css
+
+PS C:\Users\Samlite\Desktop\upwork> git branch -M main
+PS C:\Users\Samlite\Desktop\upwork> git remote add origin
